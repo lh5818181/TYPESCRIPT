d6f2248986d61e5b10a3caa63e114db88abc28b5
Luis Henrique <luish7572@gmail.com>
2024-10-23 21:29:12 -0300

    usando typescript

---
+++ src/tipos_basicos.js
@@ -0,0 +1,17 @@
+"use strict";
+let estaChovendo = false;
+estaChovendo = true;
+let idade = 27;
+let altura = 1.80;
+const nacionalidade = 'brasileira';
+const colegas = ['luis', 'paulo'];
+const tecnologias = ['html', 'css', 'js'];
+const notas = [3, 4, 5, 6];
+const lista = ['luis', true, 25];
+let idadeDaAna = 25;
+idadeDaAna = "25 anos";
+let dadosDaApi;
+dadosDaApi = 10;
+dadosDaApi = true;
+dadosDaApi = [1, 2, 3];
+dadosDaApi = 'string';
---
+++ src/tipos_basicos.ts
@@ -0,0 +1,24 @@
+let estaChovendo: boolean = false
+estaChovendo = true
+
+let idade: number = 27
+
+let altura: number = 1.80
+
+const nacionalidade: string = 'brasileira'
+
+const colegas: string[] = ['luis', 'paulo']
+const tecnologias: Array<string> = ['html', 'css', 'js']
+const notas: ReadonlyArray<number> = [3, 4, 5, 6];
+
+const lista: [nome: string, estaEstudando: boolean, idade: number] = ['luis', true, 25];
+
+let idadeDaAna: number | string = 25
+idadeDaAna = "25 anos"
+
+let dadosDaApi: any;
+dadosDaApi = 10
+dadosDaApi = true
+dadosDaApi = [1, 2, 3]
+dadosDaApi = 'string'
+
